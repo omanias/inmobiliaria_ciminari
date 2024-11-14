e35e5102bc2448d063247dd6a3ae0b0ccf1995ce
Omar J. Manias <ojmanias1985@gmail.com>
2024-11-14 12:28:21 -0300

    agrego estilos y secciones, api serverless

---
+++ js/index.js
@@ -0,0 +1,35 @@
+function updatePriceValue(value) {
+    document.getElementById("priceValue").textContent = value;
+}
+const cardContainer = document.getElementById("card-container");
+
+async function fetchProperties() {
+    try {
+        const response = await fetch("https://fr0z4mdlid.execute-api.us-west-2.amazonaws.com/getproperties");
+        if (!response.ok) throw new Error("Error al obtener los datos");
+
+        const data = await response.json();
+        const properties = data.properties;
+
+        properties.forEach(property => {
+            const card = document.createElement("div");
+            card.classList.add("card");
+
+            const title = property.name.S;
+            const price = property.price.N;
+
+            card.innerHTML = `
+                <h3>${title}</h3>
+                <p>Precio: $${price}</p>
+                <button>Ver</button>
+            `;
+
+            cardContainer.appendChild(card);
+        });
+    } catch (error) {
+        console.error("Error al cargar las propiedades:", error);
+    }
+}
+
+fetchProperties();
+
